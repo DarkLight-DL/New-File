dd6ebee850a999cc9748e69e13f9916c03676d62
DarkLight-DL <a.asikahamedm@gmail.com>
2025-09-20 18:39:39 +0530

    rout

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Nave from './SetOne/Nave';
 import Home from './SetOne/Home';
